f1d1027214cc12273c3f0a021220269a2e327493
lamberj1986 <lamberj1986@gmail.com>
2025-01-25 22:48:23 -0500

    Initial commit for week 3 lesson work.

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.json());
-app.use('/contacts', require('./routes/contacts'));
+app
+    .use(express.json())
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/contacts', require('./routes/contacts'));
 
 app.listen(process.env.PORT);
 console.log('Web Server is listening at port ' + (process.env.PORT));
